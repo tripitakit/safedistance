0181cb1801311c0e067c67d92dcabff13354fc86
patrick <patrick.demarta@gmail.com>
2025-12-21 22:39:36 +0100

    change port

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 3031,
+    host: true,
     open: true
+  },
+  preview: {
+    port: 3031,
+    host: true
   }
 })
